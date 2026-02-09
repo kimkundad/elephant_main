95df10acaf9fbbfbcfdc2ee9334f5a353dbdd3a5
kimkundad <kim.kundad@gmail.com>
2026-02-09 17:39:54 +0700

    kim

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
             $setting = \App\Models\SiteSetting::first();
             $view->with('siteSetting', $setting);
         });
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
